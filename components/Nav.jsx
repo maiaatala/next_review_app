c1ce0dbaf315489f7b384928d5dd20233773b0ca
maiaatala <ana.atala@unemat.br>
2023-11-20 14:10:30 -0400

    wip: google auth but i am using prisma and not mongoose

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,60 @@
+"use client";
+
 import React from "react";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-	return <div>Nav</div>;
+	const [isUserLogged, setIsUserLogged] = useState(true);
+	const [providers, setProviders] = useState(null);
+
+	useEffect(() => {
+		const fetchProviders = async () => {
+			const response = await getProviders();
+
+			setProviders(response);
+		};
+
+		fetchProviders();
+	}, []);
+
+	return (
+		<nav className="flex-between w-full mb-16 pt-3">
+			<Link href="/" className="flex gap-2 flex-center">
+				Home
+			</Link>
+			{/* desktop nav */}
+			<div className="sm:flex hidden">
+				{isUserLogged ? (
+					<div className="flex gap-3 md:gap-5">
+						<Link href="/classes" className="black_btn">
+							Aulas
+						</Link>
+						<Link href="/reviews" className="black_btn">
+							Avaliações
+						</Link>
+						<Link href="/professors" className="black_btn">
+							Professores
+						</Link>
+
+						<button type="button" onClick={signOut} className="outline_btn">
+							Sair
+						</button>
+					</div>
+				) : (
+					<>
+						{providers &&
+							Object.values(providers).map((provider) => (
+								<button type="button" key={provider.name} onClick={() => signIn(provider.id)} className="black_btn">
+									Logar
+								</button>
+							))}
+					</>
+				)}
+			</div>
+		</nav>
+	);
 };
 
 export default Nav;
