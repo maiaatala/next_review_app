9fb4d13c6aa8480d43caf6cdecbba9c819a23f23
maiaatala <ana.atala@unemat.br>
2023-11-20 20:55:27 -0400

    pivot

--- app/api/classes/route.js
+++ app/api/classes/route.js
@@ -1,46 +1,21 @@
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
 export const GET = async () => {
 	try {
-		const classesWithProfessorAndAverageGrade = await prisma.classes.findMany({
+		const classesWithProfessor = await prisma.classes.findMany({
 			include: {
-				professor: {
-					select: {
-						id: true,
-						name: true, // Replace with the actual field for the professor's name
-					},
-				},
-				StudentClasses: {
-					include: {
-						Feedback: {
-							select: {
-								review: true,
-							},
-						},
-					},
-				},
-			},
-			select: {
-				id: true,
-				label: true,
-				startDate: true,
-				endDate: true,
 				professor: true,
-				averageGrade: {
-					select: {
-						value: {
-							avg: {
-								review: true,
-							},
-						},
-					},
-				},
+				feedbacks: true,
 			},
 		});
 
-		return new Response(JSON.stringify(classesWithProfessorAndAverageGrade), { status: 200 });
+		return new Response(JSON.stringify(classesWithProfessor), { status: 200 });
 	} catch (error) {
-		return new Response("Failed to fetch all prompts", { status: 500 });
+		console.error("Failed to fetch classes with average feedback", error);
+		return new Response("Failed to fetch classes with average feedback", {
+			status: 500,
+		});
 	}
 };
